706a8fa98ed0ec567dcbdedffa021bcc96e313bc
Dioney Becker <Dioney.Becker@sprint.com>
2021-03-11 12:17:28 -0300

    enableeurikaclient

--- src/main/java/com/dioneybecker/beerinventoryservice/config/ServiceDiscovery.java
+++ src/main/java/com/dioneybecker/beerinventoryservice/config/ServiceDiscovery.java
@@ -2,11 +2,11 @@ package com.dioneybecker.beerinventoryservice.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("awsdev")
+
 @EnableDiscoveryClient
 @Configuration
-public class LocalDiscovery {
+public class ServiceDiscovery {
     
 }
+
